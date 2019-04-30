a6b4005a438316010c584b00e370ab4bc538d3f1
gihun2 <50095533+gihun2@users.noreply.github.com>
2019-04-30 12:37:53 +0900

    Update test_push.java

--- test_java_project/src/test_java_project/test_push.java
+++ test_java_project/src/test_java_project/test_push.java
@@ -2,7 +2,8 @@ package test_java_project;
 
 public class test_push {
 	void func() {
-		System.out.println("i'm local~!");
 		System.out.println("modify at github(server)");
+		System.out.println("i'm local~!");
+		System.out.println("pull test");
 	}
 }
